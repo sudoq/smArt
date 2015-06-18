f11470ff24d8cf8367002cf9cf7caa6596eb8638
SudoQ <simj91@gmail.com>
2015-06-18 10:47:56 +0200

    Basic data item for the classifcation

---
+++ data/data.go
@@ -0,0 +1,42 @@
+package data
+
+import (
+	_ "log"
+	"math"
+)
+
+type Data struct {
+	Attributes []float64
+	Distances []float64
+	Classification int
+}
+
+func New(attr []float64, numClass int) *Data {
+	return &Data{
+		Attributes: attr,
+		Distances: make([]float64, numClass),
+		Classification: 0,
+	}
+}
+
+func (data *Data) updateDistances(centroids []*Data){
+	for i, centroid := range(centroids){
+		sumOfSquares := 0.0;
+		for j := range(centroid.Attributes){
+			sumOfSquares += math.Pow(data.Attributes[j] - centroid.Attributes[j], 2)
+		}
+		distance := math.Sqrt(sumOfSquares)
+		data.Distances[i] = distance
+	}
+}
+
+func (data *Data) UpdateClassification(centroids []*Data){
+	data.updateDistances(centroids)
+	minDistance := math.Inf(1)
+	for i, distance := range(data.Distances) {
+		if distance < minDistance {
+			minDistance = distance
+			data.Classification = i
+		}
+	}
+}
